fa19d8a29d9a14b383d85ed12feb4c355ff32871
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-11-20 09:42:11 +0700

    Delete verify.php

--- verify.php
+++
@@ -1,3 +0,0 @@
-curl -X GET \
--H 'Authorization: Bearer {wBHvjHovGYM4KifyfM5PLgD6ypWoYU/lqofs92u2IF8zCkif9FidQseU88yTHs8cUG0egfLAS+zxGN4zOzl7yr764Z/KMq7Lch2KCT/hDRt2v6Uq/z5WLkw+DHqxluY1VKbBZw01xmmA6ohCESM6vAdB04t89/1O/w1cDnyilFU=}' \
-https://api.line.me/v1/oauth/verify
