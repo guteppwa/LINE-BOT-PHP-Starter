48682aa21e93dcc4b371b654e3d122e9f2feee9b
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-11-20 10:22:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,12 +17,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [{
-				'type' => 'text',
-				'text' => 'ไรเหย๋อ?'},
-				{'type' => 'text',
-				'text' => 'คับ?'}
-			];
+			$messages = ['type' => 'text','text' => 'ไรเหย๋อ?'],['type' => 'text','text' => 'คับ?'];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
