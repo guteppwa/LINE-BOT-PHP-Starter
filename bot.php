119a7847aad744f64364f6e4bb7c6d99cd5a92c9
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-11-20 10:37:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,9 +19,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'Hello'
+				'text' => 'Hello',
+				'type' => 'text1',
+				'text1' => 'How are you?'
 			];
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
