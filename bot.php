3f181ea9e3f71ca294e32137fce6d65199bfdec0
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-11-20 17:58:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -78,4 +78,11 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
+
+$newsXML = new SimpleXMLElement("<news></news>");
+$newsXML->addAttribute('newsPagePrefix', 'value goes here');
+$newsIntro = $newsXML->addChild('content');
+$newsIntro->addAttribute('type', 'latest');
+Header('Content-type: text/xml');
+echo $newsXML->asXML();
 echo "OK";
