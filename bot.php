3e80c2d44a3f1da59919864bac4eee098b966bac
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-11-20 17:22:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,10 +17,25 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'Hello?'
-			];
+			if($text=='ON'){
+				$messages = [
+					'type' => 'text',
+					'text' => 'LED1=ON'
+				];
+			}
+			else if($text=='OFF'){
+				$messages = [
+					'type' => 'text',
+					'text' => 'LED1=OFF'
+				];
+			}
+			else {
+				$messages = [
+					'type' => 'text',
+					'text' => 'Hello?'
+				];
+			}
+				
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
