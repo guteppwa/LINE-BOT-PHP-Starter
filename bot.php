69922fc0b5b72c84b3ebf208ec8a9ef8c499dae8
guteppwa <NapatRodtangToon25912@yahoo.com>
2016-12-14 17:31:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@ $access_token = '/NSAPdEL4qw4NIYQvP0zxZfwRvv8Cq331Z9GUO6stPjb3VqzwwxE3MVFARvEMtn
 
 // Get POST body content
 $content = file_get_contents('php://input');
-//$_path = "https://lineled.herokuapp.com/light.json";
+$_path = "https://lineled.herokuapp.com/light.json";
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
